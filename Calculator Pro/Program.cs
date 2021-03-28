0b4d03c2e0742e8fb84cd6233a5c9b294e1a6f5f
EdoardoM <mihai_eduard@mail.ru>
2021-03-28 20:12:14 +0300

    partial-classes-and-Methods

--- Calculator Pro/Program.cs
+++ Calculator Pro/Program.cs
@@ -1,9 +1,26 @@
-﻿using School;
-using System;
+﻿using System;
 using System.Threading;
 
 namespace CalculatorPro
 {
+    partial class classRoom
+    {
+        partial void classRoom1()
+        {
+            Console.WriteLine("Hello Im Classroom 10-A");
+        }
+
+        partial void classRoom2()
+        {
+            Console.WriteLine("Hello Im Class 10-B");
+        }
+
+        public void startClassRoomMethod()
+        {
+            classRoom1();
+            classRoom2();
+        }
+    }
     class MyClass
     {
         private string field = null;
@@ -38,6 +55,9 @@ namespace CalculatorPro
             fullNameChild name = new fullNameChild(nameProgram, surnameProgram, patronymicProgram);
             Console.WriteLine($"Ваше имя : {name.Name}, ваша фамилия : {name.Surname}, ваше очество : {name.Patronymic}");
 
+            classRoom classRoom = new classRoom();
+
+            classRoom.startClassRoomMethod();
 
             Console.ReadKey();
         }
---
+++ Calculator Pro/School.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorPro
+{
+    partial class classRoom
+    {
+        partial void classRoom1();
+
+        partial void classRoom2();
+    }
+    class fullNameChild
+    {
+        private string name, surname, patronymic;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string Patronymic
+        {
+            get
+            {
+                return patronymic;
+            }
+            set
+            {
+                patronymic = value;
+            }
+        }
+
+        public fullNameChild(string name, string surname, string patronymic)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+        }
+
+    }
+}
