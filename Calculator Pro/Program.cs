278e3f4c66103b4937f473dc9487d505240575d8
EdoardoM <mihai_eduard@mail.ru>
2021-04-05 21:40:24 +0300

    Enum,boxing,unboxing

--- Calculator Pro/Program.cs
+++ Calculator Pro/Program.cs
@@ -11,82 +11,18 @@ namespace CalculatorPro
         public IEnumerable<string> BasicList { get; set; }
     }
 
-    struct MyStruct
+    enum colors 
     {
-        private string firstName, secondName, patronymic;
-        private int age;
-
-        public MyStruct(string firstName, string secondName, string patronymic, int age)
-        {
-            this.firstName = firstName;
-            this.secondName = secondName;
-            this.patronymic = patronymic;
-            this.age = age;
-        }
-
-        public string MyStructName 
-        {
-            get
-            {
-                return firstName;
-            }
-            set
-            {
-                firstName = value;
-            }
-        }
-
-        public string MyStruct2Name
-        {
-            get
-            {
-                return secondName;
-            }
-            set
-            {
-                secondName = value;
-            }
-        }
-
-        public string MyStructPatronymic
-        {
-            get
-            {
-                return patronymic;
-            }
-            set
-            {
-                patronymic = value;
-            }
-        }
-
-        public int MyStructAge
-        {
-            get
-            {
-                return age;
-            }
-            set
-            {
-                age = value;
-            }
-        }
-
-        public void MyStructWrite()
-        {
-            Console.WriteLine($"Ваше имя : {firstName}");
-            Console.WriteLine($"Ваша фамилия : {secondName}");
-            Console.WriteLine($"Ваше отчество : {patronymic}");
-            if (age < 18)
-            {
-                Console.WriteLine($"Ваш возраст : {age}, вы не совершенно летний.");
-            }
-            else
-            {
-                Console.WriteLine($"Ваш возраст : {age}, вы совершенно летний");
-            }
-        }
+        White,
+        Green,
+        Blue,
+        Yellow,
+        Red,
+        Magenta, 
+        Gray
     }
+
+
 
     class Program
     {
@@ -100,18 +36,61 @@ namespace CalculatorPro
             aobj.List.Add("sdfsdf2");
             aobj.List.Add("sdfsdfsdf");
 
-            MyStruct myStruct = new MyStruct("Edward", "Mihai", "Alex", 16);
-            myStruct.MyStructWrite();
+            int i = 10;
+            //Boxing
+            object obj = i;
+            //Unboxing
+            int j = (int)obj;
 
-            MyStruct myStruct1 = new MyStruct("Danil", "Dmitrev", "Andreivici", 54);
-            myStruct1.MyStructWrite();
+            string enumColor = Console.ReadLine();
+            bool ignoreCase;
+            colors colors = (colors)Enum.Parse(typeof(colors), enumColor, ignoreCase = true);
+            Console.WriteLine(colors);
 
-            MyStruct myStruct2 = new MyStruct();
-            myStruct2.MyStructName = "Viktor";
-            myStruct2.MyStruct2Name = "Druje";
-            myStruct2.MyStructPatronymic = "Nikolaevici";
-            myStruct2.MyStructAge = 98;
-            myStruct2.MyStructWrite();
+            switch (colors)
+            {
+                case CalculatorPro.colors.White:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Magenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(colors);
+                    break;
+                case CalculatorPro.colors.Gray:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(colors);
+                    break;
+                default:
+                    break;
+            }
+
+            while (true)
+            {
+                ConsoleKey consoleKey = Console.ReadKey().Key;
+
+                int consoleKeyCode = (int)consoleKey;
+
+                Console.WriteLine($"Клавиша которую нажал : {consoleKey},\n Код этой клавиши : {consoleKeyCode}");
+            }
+
+
         }
     }
 }
