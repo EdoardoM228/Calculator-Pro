df739788e349eb1e692815e2628007db5cfd79db
EdoardoM <mihai_eduard@mail.ru>
2021-07-13 22:17:27 +0300

    Reflection

--- Calculator Pro/Program.cs
+++ Calculator Pro/Program.cs
@@ -1,71 +1,158 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Specialized;
-using System.IO;
-using System.IO.Compression;
-using System.Globalization;
-using System.Text.RegularExpressions;
-using System.Xml;
+using System.Reflection;
 
-namespace Collection
+// Рефлексия. Пример получения экземпляра Type.
+
+namespace TypeTest
 {
+    class MyClass
+    {
+    }
+
     class Program
     {
         static void Main()
         {
-            //FileStream stream = new FileStream(@"H:\Новая папка\Новый текстовый документ.xml", FileMode.Open);
+            MyClass my = new MyClass();
+            Type type;
 
-            var xmlFileWriter = new XmlTextWriter(@"H:\Новая папка\Новый текстовый документ.xml", null);
-            xmlFileWriter.Formatting = Formatting.Indented;
-            xmlFileWriter.Indentation = 1;
-            xmlFileWriter.IndentChar = ' ';
-            xmlFileWriter.QuoteChar = '\"';  
+            /* Способы получения экземрляра класса Type. */
 
-            xmlFileWriter.WriteStartDocument();
-            xmlFileWriter.WriteStartElement("ListOfBook");
-            xmlFileWriter.WriteStartElement("Book");
-            xmlFileWriter.WriteStartElement("Title");
-            xmlFileWriter.WriteString(" Dubrovskii");
-            xmlFileWriter.WriteComment("навзание книги");
-            xmlFileWriter.WriteEndElement();
-            xmlFileWriter.WriteStartElement("Author");
-            xmlFileWriter.WriteString(" A.S.Puskin");
-            xmlFileWriter.WriteComment("Автор книги");
-            xmlFileWriter.WriteEndElement();
-            xmlFileWriter.WriteStartElement("Year");
-            xmlFileWriter.WriteString(" 1841");
-            xmlFileWriter.WriteComment("Год первого выпуска");
-            xmlFileWriter.WriteEndElement();
-            xmlFileWriter.WriteStartElement("theMainCharacter");
-            xmlFileWriter.WriteString(@"Троекуров Кирила Петрович
-Дубровский - старший
-Владимир Дубровский
-Маша Троекурова
-Князь Верейский
-Антон Спицын");
-            xmlFileWriter.WriteEndElement();
-            xmlFileWriter.WriteEndElement();
-            xmlFileWriter.WriteEndElement();
+            // 1.
+            type = my.GetType();
+            Console.WriteLine("1й способ: " + type);
 
-            xmlFileWriter.Close();
+            // 2.
+            // Полное квалифицированое имя типа в строковом представлении.
+            type = Type.GetType("TypeTest.MyClass");
+            Console.WriteLine("2й способ: " + type);
 
-            var xmlDocument = new XmlDocument();
-            xmlDocument.Load(@"H:\Новая папка\Новый текстовый документ.xml");
+            // 3.
+            type = typeof(MyClass);
+            Console.WriteLine("3й способ: " + type);
 
-            XmlNode root = xmlDocument.DocumentElement;
 
-            Console.WriteLine($"Имя документа : {xmlDocument.LocalName}");
-
-            foreach(XmlNode book in root.ChildNodes)
+            // Получаем разную информацию о Class1.
+            static void ListVariosStats(Class1 cl)
             {
-                Console.WriteLine("Найденная книга : ");
-                foreach(XmlNode species in book.ChildNodes)
-                {
-                    Console.WriteLine(book.Name + ": " + book.InnerText);
-                }
+                Console.WriteLine(new string('_', 30) + " Информация о Class1" + "\n");
+                Type t = cl.GetType();
+
+                Console.WriteLine("Полное Имя:             {0}", t.FullName);
+                Console.WriteLine("Базовый класс:          {0}", t.BaseType);
+                Console.WriteLine("Абстрактный:            {0}", t.IsAbstract);
+                Console.WriteLine("Это COM объект:         {0}", t.IsCOMObject);
+                Console.WriteLine("Запрещено наследование: {0}", t.IsSealed);
+                Console.WriteLine("Это class:              {0}", t.IsClass);
             }
 
+            // Получаем информацию об Именах всех методов Class1.
+            static void ListMethods(Class1 cl)
+            {
+                Console.WriteLine(new string('_', 30) + " Методы класса Class1" + "\n");
+
+                Type t = cl.GetType();
+                MethodInfo[] mi = t.GetMethods(BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+
+                foreach (MethodInfo m in mi)
+                    Console.WriteLine("Method: {0}", m.Name);
+            }
+
+            // Получаем информацию об Именах полей Class1.
+            static void ListFields(Class1 cl)
+            {
+                Console.WriteLine(new string('_', 30) + " Поля класса Class1" + "\n");
+
+                Type t = cl.GetType();
+                FieldInfo[] fi =
+                    t.GetFields(BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic);
+
+                foreach (FieldInfo f in fi)
+                    Console.WriteLine("Field: {0}", f.Name);
+            }
+
+            // Получаем список всех Свойств Class1.
+            static void ListProps(Class1 cl)
+            {
+                Console.WriteLine(new string('_', 30) + " Свойства класса Class1" + "\n");
+
+                Type t = cl.GetType();
+                PropertyInfo[] pi = t.GetProperties();
+
+                foreach (PropertyInfo p in pi)
+                    Console.WriteLine("Свойство: {0}", p.Name);
+            }
+
+            // Получаем список всех Интерфейсов, поддерживаемых Class1.
+            static void ListInterfaces(Class1 cl)
+            {
+                Console.WriteLine(new string('_', 30) + " Интерфейсы класса Class1" + "\n");
+
+                Type t = cl.GetType();
+
+                Type[] it = t.GetInterfaces();
+
+                foreach (Type i in it)
+                    Console.WriteLine("Интерфейс: {0}", i.Name);
+            }
+
+            // Получаем информацию обо всех конструкторах Class1.
+            static void ListConstructors(Class1 cl)
+            {
+                Console.WriteLine(new string('_', 30) + " Конструкторы класса Class1" + "\n");
+
+                Type t = cl.GetType();
+                ConstructorInfo[] ci = t.GetConstructors();
+
+                foreach (ConstructorInfo m in ci)
+                    Console.WriteLine("Constructor: {0}", m.Name);
+            }
+
+            static void Main()
+            {
+                Console.SetWindowSize(80, 45);
+
+                Class1 instance = new Class1();
+
+                #region Вывод информации о типе
+
+                ListVariosStats(instance);  // Получаем разную информацию о Class1.
+                ListMethods(instance);      // Получаем информацию об Именах всех методов Class1.
+                ListFields(instance);       // Получаем информацию об Именах всех полей Class1.
+                ListProps(instance);        // Получаем список всех Свойств Class1.
+                ListInterfaces(instance);   // Получаем список всех Интерфейсов, поддерживаемых Class1.
+                ListConstructors(instance); // Получаем информацию об Именах всех конструкторов Class1.
+
+                #endregion
+
+                #region Обращение к закрытым членам.
+
+                Console.WriteLine(new string('-', 60));
+
+                Type type = instance.GetType();
+
+                // Вызов private метода
+                MethodInfo methodC = type.GetMethod("MethodC", BindingFlags.Instance | BindingFlags.NonPublic);
+                methodC.Invoke(instance, new object[] { "Hello", " world!" });
+
+                // Запись значения в private поле
+                FieldInfo mystring = type.GetField("mystring", BindingFlags.Instance | BindingFlags.NonPublic);
+                mystring.SetValue(instance, "Привет Мир!");
+
+                Console.WriteLine(instance.MyString);
+
+                #endregion
+            }
+
+            // Delay.
             Console.ReadKey();
+
         }
     }
 }
--- Calculator Pro/School.cs
+++ Calculator Pro/School.cs
@@ -5,94 +5,53 @@ using System.Linq;
 
 // ICollection<T> - определяет методы, используемые для управления универсальными коллекциями.
 
-namespace Collection
+namespace TypeTest
 {
-    public class Persone
+    public class Class1 : IInterface1, IInterface2
     {
-        public string fName;
-        public string sName;
+        // Поля.         
+        public int myint;
+        private string mystring = "Hello";
 
-        public Persone(string f , string s)
+        // Конструкторы.
+        public Class1() { }
+        public Class1(int i)
         {
-            fName = f;
-            sName = s;
+            this.myint = i;
         }
-    }    
 
-    public class People : IEnumerable
+        // Свойства.
+        public int myProp
+        {
+            get { return myint; }
+            set { myint = value; }
+        }
+
+        public string MyString
+        {
+            get { return mystring; }
+        }
+
+        // Методы.
+        public void MethodA() { }
+        public void MethodB() { }
+
+        private void MethodC(string str, string str2)
+        {
+            Console.WriteLine(str + str2);
+        }
+
+        public void myMethod(int p1, string p2) { }
+    }
+    public interface IInterface1
     {
-        private Persone[] persone;
-
-        public People(Persone[] personeArray)
-        {
-            persone = new Persone[personeArray.Length];
-
-            for(int i = 0; i < personeArray.Length; i++)
-            {
-                persone[i] = personeArray[i];
-            }   
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public peopleEnum GetEnumerator()
-        {
-            return new peopleEnum(persone);
-        }
-
+        void MethodA();
     }
 
-    public class peopleEnum : IEnumerator
+    public interface IInterface2
     {
-        public Persone[] persone;
-
-        int position = -1;
-
-        public peopleEnum(Persone[] listPersone)
-        {
-            persone = listPersone;
-        }
-
-        public bool MoveNext()
-        {
-            position++;
-            return (position < persone.Length);
-        }
-
-        public void Reset()
-        {
-            position = -1;
-        }
-
-        public Persone Current
-        {
-            get
-            {
-                try
-                {
-                    return persone[position];
-                }
-                catch(IndexOutOfRangeException)
-                {
-                    throw new InvalidOperationException();
-                }
-                finally
-                {
-                    Console.WriteLine("Завершение");
-                }
-            }
-        }
-
-
-        object IEnumerator.Current
-        {
-            get
-            {
-                return Current;
-            }
-        }
+        void MethodB();
     }
+
+
 }
